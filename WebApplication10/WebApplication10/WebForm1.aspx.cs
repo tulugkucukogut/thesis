e1f1579be8096102249930ddf32dbfd88328e694
tulugkucukogut <38864788+tulugkucukogut@users.noreply.github.com>
2018-05-02 21:28:12 +0300

    first commit

---
+++ WebApplication10/WebApplication10/WebForm1.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WebApplication10
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["dataconnect"].ConnectionString);
+                con.Open();
+                string insert = "insert into dataTable (Data) values(@Data)";
+                SqlCommand cmd = new SqlCommand(insert, con);
+                cmd.Parameters.AddWithValue("@Data", TextBox1.Text);
+       
+                cmd.ExecuteNonQuery();
+
+                Response.Redirect("WebForm2.aspx");
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex);
+            }
+        }
+    }
+}
---
+++ WebApplication10/WebApplication10/WebForm2.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="WebApplication10.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:dataconnect %>" SelectCommand="SELECT * FROM [dataTable]"></asp:SqlDataSource>
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="Id" DataSourceID="SqlDataSource1">
+            <Columns>
+                <asp:BoundField DataField="Id" HeaderText="Id" ReadOnly="True" SortExpression="Id" />
+                <asp:BoundField DataField="Data" HeaderText="Data" SortExpression="Data" />
+            </Columns>
+        </asp:GridView>
+    </form>
+</body>
+</html>
